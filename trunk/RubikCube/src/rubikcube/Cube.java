c95b4ca1ade0ad5a745f4e152590f635e19eec3d
wjgranger5@gmail.com <wjgranger5@gmail.com@a19310ab-6519-9718-2636-1e6ddd72dc8a>
2010-10-07 17:46:46 +0000

    changed the indexing back to 0 to 8. It was creating special characters in the print statement

--- trunk/RubikCube/src/rubikcube/Cube.java
+++ trunk/RubikCube/src/rubikcube/Cube.java
@@ -16,11 +16,7 @@ public class Cube {
     private final static int ORANGE = 3;
     private final static int BLUE = 4;
     private final static int WHITE = 5;
-    private char[][] cubic = new char[6][10]; // Blaine - 1 unused location cubic[color][0] 
-                                              // but allows addressing as cubic[color][1-9]
-                                              // corresponding directly to cube model we have.
-                                              // Effectively makes it easier to visualize
-                                              // the array exactly as the paper cube appears.
+    private char[][] cubic = new char[6][9];
 
     private int FRONT;
     private int BACK;
@@ -38,7 +34,7 @@ public class Cube {
         char[] CubeInChar = CubeInText.toCharArray();
         int k = 0;
         for(int i = 0; i < 6; i++) {
-            for(int j = 1; j <= 9; j++) {
+            for(int j = 0; j < 9; j++) {
                 cubic[i][j] = CubeInChar[k];
                 k++;
             }
@@ -56,8 +52,8 @@ public class Cube {
         } else {
             char[] CubeInChar = CubeInText.toCharArray();
             int k = 0;
-            for (int i = 1; i <= 6; i++) {
-                for (int j = 1; j <= 9; j++) {
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 9; j++) {
                     cubic[i][j] = CubeInChar[k];
                     k++;
                 }
@@ -128,29 +124,29 @@ public class Cube {
     {
       if( dir.equals("cw") )
       {
-        char t2 = cubic[face][2];
-        char t3 = cubic[face][3];
-        cubic[face][3] = cubic[face][1];
-        cubic[face][2] = cubic[face][4];
-        cubic[face][1] = cubic[face][7];
-        cubic[face][4] = cubic[face][8];
-        cubic[face][7] = cubic[face][9];
-        cubic[face][8] = cubic[face][6];
-        cubic[face][9] = t3;
-        cubic[face][6] = t2; 
+        char t2 = cubic[face][1];
+        char t3 = cubic[face][2];
+        cubic[face][2] = cubic[face][0];
+        cubic[face][1] = cubic[face][3];
+        cubic[face][0] = cubic[face][6];
+        cubic[face][3] = cubic[face][7];
+        cubic[face][6] = cubic[face][8];
+        cubic[face][7] = cubic[face][5];
+        cubic[face][8] = t3;
+        cubic[face][5] = t2;
       }
       else if( dir.equals("ccw") )
       {
-        char t1 = cubic[face][1];
-        char t2 = cubic[face][2];
-        cubic[face][1] = cubic[face][3];
-        cubic[face][2] = cubic[face][6];
-        cubic[face][3] = cubic[face][9];
-        cubic[face][6] = cubic[face][8];
-        cubic[face][9] = cubic[face][7];
-        cubic[face][8] = cubic[face][4];
-        cubic[face][7] = t1;
-        cubic[face][4] = t2;
+        char t1 = cubic[face][0];
+        char t2 = cubic[face][1];
+        cubic[face][0] = cubic[face][2];
+        cubic[face][1] = cubic[face][5];
+        cubic[face][2] = cubic[face][8];
+        cubic[face][5] = cubic[face][7];
+        cubic[face][8] = cubic[face][6];
+        cubic[face][7] = cubic[face][3];
+        cubic[face][6] = t1;
+        cubic[face][3] = t2;
       }
     }
     
@@ -188,39 +184,39 @@ public class Cube {
     {
     	//System.out.println("Rotate Yellow " + dir );
     	
-    	char t1 = cubic[BLUE][1];
-        char t2 = cubic[BLUE][2];
-        char t3 = cubic[BLUE][3];
+    	char t1 = cubic[BLUE][0];
+        char t2 = cubic[BLUE][1];
+        char t3 = cubic[BLUE][2];
     	
     	if( dir.equals("cw") )
         {
+          cubic[BLUE][0] = cubic[RED][0];
           cubic[BLUE][1] = cubic[RED][1];
           cubic[BLUE][2] = cubic[RED][2];
-          cubic[BLUE][3] = cubic[RED][3];
+          cubic[RED][0] = cubic[GREEN][0];
           cubic[RED][1] = cubic[GREEN][1];
           cubic[RED][2] = cubic[GREEN][2];
-          cubic[RED][3] = cubic[GREEN][3];
+          cubic[GREEN][0] = cubic[ORANGE][0];
           cubic[GREEN][1] = cubic[ORANGE][1];
           cubic[GREEN][2] = cubic[ORANGE][2];
-          cubic[GREEN][3] = cubic[ORANGE][3];
-          cubic[ORANGE][1] = t1;
-          cubic[ORANGE][2] = t2;
-          cubic[ORANGE][3] = t3;
+          cubic[ORANGE][0] = t1;
+          cubic[ORANGE][1] = t2;
+          cubic[ORANGE][2] = t3;
         }
         else if( dir.equals("ccw") )
         {
-          cubic[BLUE][3] = cubic[ORANGE][3];
           cubic[BLUE][2] = cubic[ORANGE][2];
           cubic[BLUE][1] = cubic[ORANGE][1];
-          cubic[ORANGE][3] = cubic[GREEN][3];
+          cubic[BLUE][0] = cubic[ORANGE][0];
           cubic[ORANGE][2] = cubic[GREEN][2];
           cubic[ORANGE][1] = cubic[GREEN][1];
-          cubic[GREEN][3] = cubic[RED][3];
+          cubic[ORANGE][0] = cubic[GREEN][0];
           cubic[GREEN][2] = cubic[RED][2];
           cubic[GREEN][1] = cubic[RED][1];
-          cubic[RED][3] = t3;
-          cubic[RED][2] = t2;
-          cubic[RED][1] = t1;
+          cubic[GREEN][0] = cubic[RED][0];
+          cubic[RED][2] = t3;
+          cubic[RED][1] = t2;
+          cubic[RED][0] = t1;
         }
     }
     
@@ -228,39 +224,39 @@ public class Cube {
     {
     	//System.out.println("Rotate Red " + dir );
     	
-    	char t1 = cubic[YELLOW][1];
-        char t4 = cubic[YELLOW][4];
-        char t7 = cubic[YELLOW][7];
+    	char t1 = cubic[YELLOW][0];
+        char t4 = cubic[YELLOW][3];
+        char t7 = cubic[YELLOW][6];
     	
     	if( dir.equals("cw") )
         {
-    		cubic[YELLOW][7] = cubic[BLUE][3];
-    		cubic[YELLOW][4] = cubic[BLUE][6];
-    		cubic[YELLOW][1] = cubic[BLUE][9];
-    		cubic[BLUE][3] = cubic[WHITE][7];
-    		cubic[BLUE][6] = cubic[WHITE][4];
-    		cubic[BLUE][9] = cubic[WHITE][1];
-    		cubic[WHITE][7] = cubic[GREEN][7];
-    		cubic[WHITE][4] = cubic[GREEN][4];
-    		cubic[WHITE][1] = cubic[GREEN][1];
-    		cubic[GREEN][7] = t7;
-    		cubic[GREEN][4] = t4;
-    		cubic[GREEN][1] = t1;
+    		cubic[YELLOW][6] = cubic[BLUE][2];
+    		cubic[YELLOW][3] = cubic[BLUE][5];
+    		cubic[YELLOW][0] = cubic[BLUE][8];
+    		cubic[BLUE][2] = cubic[WHITE][6];
+    		cubic[BLUE][5] = cubic[WHITE][3];
+    		cubic[BLUE][8] = cubic[WHITE][0];
+    		cubic[WHITE][6] = cubic[GREEN][6];
+    		cubic[WHITE][3] = cubic[GREEN][3];
+    		cubic[WHITE][0] = cubic[GREEN][0];
+    		cubic[GREEN][6] = t7;
+    		cubic[GREEN][3] = t4;
+    		cubic[GREEN][0] = t1;
         }
         else if( dir.equals("ccw") )
         {
-        	cubic[YELLOW][1] = cubic[GREEN][1];
-        	cubic[YELLOW][4] = cubic[GREEN][4];
-        	cubic[YELLOW][7] = cubic[GREEN][7];
-        	cubic[GREEN][1] = cubic[WHITE][1];
-        	cubic[GREEN][4] = cubic[WHITE][4];
-        	cubic[GREEN][7] = cubic[WHITE][7];
-        	cubic[WHITE][1] = cubic[BLUE][9];
-        	cubic[WHITE][4] = cubic[BLUE][6];
-        	cubic[WHITE][7] = cubic[BLUE][3];
-        	cubic[BLUE][9] = t1;
-        	cubic[BLUE][6] = t4;
-        	cubic[BLUE][3] = t7;
+        	cubic[YELLOW][0] = cubic[GREEN][0];
+        	cubic[YELLOW][3] = cubic[GREEN][3];
+        	cubic[YELLOW][6] = cubic[GREEN][6];
+        	cubic[GREEN][0] = cubic[WHITE][0];
+        	cubic[GREEN][3] = cubic[WHITE][3];
+        	cubic[GREEN][6] = cubic[WHITE][6];
+        	cubic[WHITE][0] = cubic[BLUE][8];
+        	cubic[WHITE][3] = cubic[BLUE][5];
+        	cubic[WHITE][6] = cubic[BLUE][2];
+        	cubic[BLUE][8] = t1;
+        	cubic[BLUE][5] = t4;
+        	cubic[BLUE][2] = t7;
         }
     }
     
@@ -268,39 +264,39 @@ public class Cube {
     {
     	//System.out.println("Rotate Green " + dir );
     	
-    	char t7 = cubic[YELLOW][7];
-        char t8 = cubic[YELLOW][8];
-        char t9 = cubic[YELLOW][9];
+    	char t7 = cubic[YELLOW][6];
+        char t8 = cubic[YELLOW][7];
+        char t9 = cubic[YELLOW][8];
     	
     	if( dir.equals("cw") )
         {
-    		cubic[YELLOW][9] = cubic[RED][3];
-    		cubic[YELLOW][8] = cubic[RED][6];
-    		cubic[YELLOW][7] = cubic[RED][9];
-    		cubic[RED][3] = cubic[WHITE][1];
-    		cubic[RED][6] = cubic[WHITE][2];
-    		cubic[RED][9] = cubic[WHITE][3];
-    		cubic[WHITE][1] = cubic[ORANGE][7];
-    		cubic[WHITE][2] = cubic[ORANGE][4];
-    		cubic[WHITE][3] = cubic[ORANGE][1];
-    		cubic[ORANGE][7] = t9;
-    		cubic[ORANGE][4] = t8;
-    		cubic[ORANGE][1] = t7;
+    		cubic[YELLOW][8] = cubic[RED][2];
+    		cubic[YELLOW][7] = cubic[RED][5];
+    		cubic[YELLOW][6] = cubic[RED][8];
+    		cubic[RED][2] = cubic[WHITE][0];
+    		cubic[RED][5] = cubic[WHITE][1];
+    		cubic[RED][8] = cubic[WHITE][2];
+    		cubic[WHITE][0] = cubic[ORANGE][6];
+    		cubic[WHITE][1] = cubic[ORANGE][3];
+    		cubic[WHITE][2] = cubic[ORANGE][0];
+    		cubic[ORANGE][6] = t9;
+    		cubic[ORANGE][3] = t8;
+    		cubic[ORANGE][0] = t7;
         }
         else if( dir.equals("ccw") )
         {
-        	cubic[YELLOW][7] = cubic[ORANGE][1];
-        	cubic[YELLOW][8] = cubic[ORANGE][4];
-        	cubic[YELLOW][9] = cubic[ORANGE][7];
-        	cubic[ORANGE][1] = cubic[WHITE][3];
-        	cubic[ORANGE][4] = cubic[WHITE][2];
-        	cubic[ORANGE][7] = cubic[WHITE][1];
-        	cubic[WHITE][3] = cubic[RED][9];
-        	cubic[WHITE][2] = cubic[RED][6];
-        	cubic[WHITE][1] = cubic[RED][3];
-        	cubic[RED][9] = t7;
-        	cubic[RED][6] = t8;
-        	cubic[RED][3] = t9;
+        	cubic[YELLOW][6] = cubic[ORANGE][0];
+        	cubic[YELLOW][7] = cubic[ORANGE][3];
+        	cubic[YELLOW][8] = cubic[ORANGE][6];
+        	cubic[ORANGE][0] = cubic[WHITE][2];
+        	cubic[ORANGE][3] = cubic[WHITE][1];
+        	cubic[ORANGE][6] = cubic[WHITE][0];
+        	cubic[WHITE][2] = cubic[RED][8];
+        	cubic[WHITE][1] = cubic[RED][5];
+        	cubic[WHITE][0] = cubic[RED][2];
+        	cubic[RED][8] = t7;
+        	cubic[RED][5] = t8;
+        	cubic[RED][2] = t9;
         }
     }
     
@@ -308,39 +304,39 @@ public class Cube {
     {
     	//System.out.println("Rotate Orange " + dir );
     	
-    	char t3 = cubic[YELLOW][3];
-        char t6 = cubic[YELLOW][6];
-        char t9 = cubic[YELLOW][9];
+    	char t3 = cubic[YELLOW][2];
+        char t6 = cubic[YELLOW][5];
+        char t9 = cubic[YELLOW][8];
     	
     	if( dir.equals("cw") )
         {
-    		cubic[YELLOW][3] = cubic[GREEN][3];
-    		cubic[YELLOW][6] = cubic[GREEN][6];
-    		cubic[YELLOW][9] = cubic[GREEN][9];
-    		cubic[GREEN][3] = cubic[WHITE][3];
-    		cubic[GREEN][6] = cubic[WHITE][6];
-    		cubic[GREEN][9] = cubic[WHITE][9];
-    		cubic[WHITE][3] = cubic[BLUE][7];
-    		cubic[WHITE][6] = cubic[BLUE][4];
-    		cubic[WHITE][9] = cubic[BLUE][1];
-    		cubic[BLUE][7] = t3;
-    		cubic[BLUE][4] = t6;
-    		cubic[BLUE][1] = t9;
+    		cubic[YELLOW][2] = cubic[GREEN][2];
+    		cubic[YELLOW][5] = cubic[GREEN][5];
+    		cubic[YELLOW][8] = cubic[GREEN][8];
+    		cubic[GREEN][2] = cubic[WHITE][2];
+    		cubic[GREEN][5] = cubic[WHITE][5];
+    		cubic[GREEN][8] = cubic[WHITE][8];
+    		cubic[WHITE][2] = cubic[BLUE][6];
+    		cubic[WHITE][5] = cubic[BLUE][3];
+    		cubic[WHITE][8] = cubic[BLUE][0];
+    		cubic[BLUE][6] = t3;
+    		cubic[BLUE][3] = t6;
+    		cubic[BLUE][0] = t9;
         }
         else if( dir.equals("ccw") )
         {
-        	cubic[YELLOW][9] = cubic[BLUE][1];
-        	cubic[YELLOW][6] = cubic[BLUE][4];
-        	cubic[YELLOW][3] = cubic[BLUE][7];
-        	cubic[BLUE][1] = cubic[WHITE][9];
-        	cubic[BLUE][4] = cubic[WHITE][6];
-        	cubic[BLUE][7] = cubic[WHITE][3];
-        	cubic[WHITE][9] = cubic[GREEN][9];
-        	cubic[WHITE][6] = cubic[GREEN][6];
-        	cubic[WHITE][3] = cubic[GREEN][3];
-        	cubic[GREEN][9] = t9;
-        	cubic[GREEN][6] = t6;
-        	cubic[GREEN][3] = t3;
+        	cubic[YELLOW][8] = cubic[BLUE][0];
+        	cubic[YELLOW][5] = cubic[BLUE][3];
+        	cubic[YELLOW][2] = cubic[BLUE][6];
+        	cubic[BLUE][0] = cubic[WHITE][8];
+        	cubic[BLUE][3] = cubic[WHITE][5];
+        	cubic[BLUE][6] = cubic[WHITE][2];
+        	cubic[WHITE][8] = cubic[GREEN][8];
+        	cubic[WHITE][5] = cubic[GREEN][5];
+        	cubic[WHITE][2] = cubic[GREEN][2];
+        	cubic[GREEN][8] = t9;
+        	cubic[GREEN][5] = t6;
+        	cubic[GREEN][2] = t3;
         }
     }
     
@@ -348,39 +344,39 @@ public class Cube {
     {
     	//System.out.println("Rotate Blue " + dir );
     	
-    	char t1 = cubic[YELLOW][1];
-        char t2 = cubic[YELLOW][2];
-        char t3 = cubic[YELLOW][3];
+    	char t1 = cubic[YELLOW][0];
+        char t2 = cubic[YELLOW][1];
+        char t3 = cubic[YELLOW][2];
     	
     	if( dir.equals("cw") )
         {
-    		cubic[YELLOW][1] = cubic[ORANGE][3];
-    		cubic[YELLOW][2] = cubic[ORANGE][6];
-    		cubic[YELLOW][3] = cubic[ORANGE][9];
-    		cubic[ORANGE][3] = cubic[WHITE][9];
-    		cubic[ORANGE][6] = cubic[WHITE][8];
-    		cubic[ORANGE][9] = cubic[WHITE][7];
-    		cubic[WHITE][9] = cubic[RED][7];
-    		cubic[WHITE][8] = cubic[RED][4];
-    		cubic[WHITE][7] = cubic[RED][1];
-    		cubic[RED][7] = t1;
-    		cubic[RED][4] = t2;
-    		cubic[RED][1] = t3;
+    		cubic[YELLOW][0] = cubic[ORANGE][2];
+    		cubic[YELLOW][1] = cubic[ORANGE][5];
+    		cubic[YELLOW][2] = cubic[ORANGE][8];
+    		cubic[ORANGE][2] = cubic[WHITE][8];
+    		cubic[ORANGE][5] = cubic[WHITE][7];
+    		cubic[ORANGE][8] = cubic[WHITE][6];
+    		cubic[WHITE][8] = cubic[RED][6];
+    		cubic[WHITE][7] = cubic[RED][3];
+    		cubic[WHITE][6] = cubic[RED][0];
+    		cubic[RED][6] = t1;
+    		cubic[RED][3] = t2;
+    		cubic[RED][0] = t3;
         }
         else if( dir.equals("ccw") )
         {
-        	cubic[YELLOW][3] = cubic[RED][1];
-        	cubic[YELLOW][2] = cubic[RED][4];
-        	cubic[YELLOW][1] = cubic[RED][7];
-        	cubic[RED][1] = cubic[WHITE][7];
-        	cubic[RED][4] = cubic[WHITE][8];
-        	cubic[RED][7] = cubic[WHITE][9];
-        	cubic[WHITE][7] = cubic[ORANGE][9];
-        	cubic[WHITE][8] = cubic[ORANGE][6];
-        	cubic[WHITE][9] = cubic[ORANGE][3];
-        	cubic[ORANGE][9] = t3;
-        	cubic[ORANGE][6] = t2;
-        	cubic[ORANGE][3] = t1;
+        	cubic[YELLOW][2] = cubic[RED][0];
+        	cubic[YELLOW][1] = cubic[RED][3];
+        	cubic[YELLOW][0] = cubic[RED][6];
+        	cubic[RED][0] = cubic[WHITE][6];
+        	cubic[RED][3] = cubic[WHITE][7];
+        	cubic[RED][6] = cubic[WHITE][8];
+        	cubic[WHITE][6] = cubic[ORANGE][8];
+        	cubic[WHITE][7] = cubic[ORANGE][5];
+        	cubic[WHITE][8] = cubic[ORANGE][2];
+        	cubic[ORANGE][8] = t3;
+        	cubic[ORANGE][5] = t2;
+        	cubic[ORANGE][2] = t1;
         }
     }
     
@@ -388,39 +384,39 @@ public class Cube {
     {
     	//System.out.println("Rotate White " + dir );
     	
-    	char t7 = cubic[GREEN][7];
-        char t8 = cubic[GREEN][8];
-        char t9 = cubic[GREEN][9];
+    	char t7 = cubic[GREEN][6];
+        char t8 = cubic[GREEN][7];
+        char t9 = cubic[GREEN][8];
     	
     	if( dir.equals("cw") )
         {
-    		cubic[GREEN][9] = cubic[RED][9];
     		cubic[GREEN][8] = cubic[RED][8];
     		cubic[GREEN][7] = cubic[RED][7];
-    		cubic[RED][9] = cubic[BLUE][9];
+    		cubic[GREEN][6] = cubic[RED][6];
     		cubic[RED][8] = cubic[BLUE][8];
     		cubic[RED][7] = cubic[BLUE][7];
-    		cubic[BLUE][9] = cubic[ORANGE][9];
+    		cubic[RED][6] = cubic[BLUE][6];
     		cubic[BLUE][8] = cubic[ORANGE][8];
     		cubic[BLUE][7] = cubic[ORANGE][7];
-    		cubic[ORANGE][9] = t9;
-    		cubic[ORANGE][8] = t8;
-    		cubic[ORANGE][7] = t7;
+    		cubic[BLUE][6] = cubic[ORANGE][6];
+    		cubic[ORANGE][8] = t9;
+    		cubic[ORANGE][7] = t8;
+    		cubic[ORANGE][6] = t7;
         }
         else if( dir.equals("ccw") )
         {
+        	cubic[GREEN][6] = cubic[ORANGE][6];
         	cubic[GREEN][7] = cubic[ORANGE][7];
         	cubic[GREEN][8] = cubic[ORANGE][8];
-        	cubic[GREEN][9] = cubic[ORANGE][9];
+        	cubic[ORANGE][6] = cubic[BLUE][6];
         	cubic[ORANGE][7] = cubic[BLUE][7];
         	cubic[ORANGE][8] = cubic[BLUE][8];
-        	cubic[ORANGE][9] = cubic[BLUE][9];
+        	cubic[BLUE][6] = cubic[RED][6];
         	cubic[BLUE][7] = cubic[RED][7];
         	cubic[BLUE][8] = cubic[RED][8];
-        	cubic[BLUE][9] = cubic[RED][9];
-        	cubic[RED][7] = t7;
-        	cubic[RED][8] = t8;
-        	cubic[RED][9] = t9;
+        	cubic[RED][6] = t7;
+        	cubic[RED][7] = t8;
+        	cubic[RED][8] = t9;
         }
     }
     
