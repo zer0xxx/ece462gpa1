06f2a62d0df2b08180a824cca9f990c8dfa09cfd
wjgranger5@gmail.com <wjgranger5@gmail.com@a19310ab-6519-9718-2636-1e6ddd72dc8a>
2010-10-10 21:01:30 +0000

    change main to GPA1

---
+++ RubikCube/src/rubikcube/GPA1.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rubikcube;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author wgranger
+ */
+public class GPA1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: <file>");
+            System.exit(0);
+        }
+        try {
+            // Open the file that is the first 
+            // command line parameter
+            FileInputStream fstream = new FileInputStream(args[0]);
+            // Get the object of DataInputStream
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            Cube rubik = new Cube();
+            //Read File Line By Line
+            while ((strLine = br.readLine()) != null) {
+                rubik.manipulate(strLine);
+            }
+            //Close the input stream
+            in.close();
+        } catch (Exception e) {//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
+
+    }
+}
